d3e993cf483c69261bd7f6e44f2db67c8173383c
rriverod <rriverod@chicagobooth.edu>
2025-02-27 04:53:51 +0000

    grade

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,10 +22,10 @@ class FollowRequest < ApplicationRecord
   ## Direct associations
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
-  belongs_to(:sender, foreign_key:"sender_id", User)
+  belongs_to(:sender, class_name: "User", foreign_key:"sender_id")
 
 
   # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
-  belongs_to(:recipient, foreign_key:"recipient_id", User)
+  belongs_to(:recipient, class_name: "User", foreign_key:"recipient_id")
 
 end
